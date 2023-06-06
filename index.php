6fb4754a6c77ea3ced0c8d470cb1566e019872b1
Chris Paschall <ChrisP1108@gmail.com>
2023-06-05 20:53:55 -0700

    Favicon Added And Title

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
     <?php endif; ?>
 
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Podcast Player</title>
+    <link rel="icon" type="image/jpeg" href="<?php echo $podcast_image; ?>">
+    <title><?php echo $parsed_rss_feed->channel->title; ?></title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=<?php echo $style_font; ?>:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
         * {
