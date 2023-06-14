ef86c437b6eacd177af4f3b1dc4c37c2e267694e
Chris Paschall <ChrisP1108@gmail.com>
2023-06-13 22:41:04 -0700

    Single Episode Mode Integration

---
+++ code-generator/index.php
@@ -0,0 +1,112 @@
+<?php 
+    include 'fonts/fonts.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" class="pseudo">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Podcast Player Code Generator</title>
+</head>
+<body class="pseudo">
+    <h1>Podcast Player Code Generator</h1>
+    <div class="content pseudo">
+        <div class="fields-preview-row">
+            <main class="column pseudo">
+                <h3>Podcast Feed / Styling</h3>
+                <form class="field-sections frame-shadow pseudo"> 
+                    <section class="podcast-url-section">
+                        <label for="url">Apple Podcast Url</label>
+                        <input name="url" id="url" type="text">
+                        <button id="clear-rss-button" class="clear-url-button pseudo">Clear Url Field</button>
+                    </section>
+                    <section class="font-field-section">
+                        <label for="font">Font Family <br>(Google Fonts)</label>
+                        <div class="font-field-container">
+                            <input name="font" id="font" type="text">
+                            <ol id="font-list-output"></ol>
+                        </div>
+                    </section>
+                    <section>
+                        <label for="mode">Mode</label>
+                        <select name="mode" id="mode">
+                            <option value="light">Light</option>
+                            <option value="dark">Dark</option>
+                        </select>
+                    </section>
+                    <section>
+                        <label for="color1">Theme Color</label>
+                        <input name="color1" id="color1" type="color">
+                    </section>
+                    <section>
+                        <label for="textcolor">Text Color</label>
+                        <input name="textcolor" id="textcolor" type="color">
+                    </section>
+                    <section>
+                        <label for="progressbarcolor">Progress Bar Color</label>
+                        <input name="progressbarcolor" id="progressbarcolor" type="color">
+                    </section>
+                    <section>
+                        <label for="buttoncolor">Play/Pause Button Color</label>
+                        <input name="buttoncolor" id="buttoncolor" type="color">
+                    </section>
+                    <section>
+                        <label for="highlightcolor">Selected Episode Highlight Color</label>
+                        <input name="highlightcolor" id="highlightcolor" type="color">
+                    </section>
+                    <section>
+                        <label for="scrollcolor">Scroll Bar Thumb Color</label>
+                        <input name="scrollcolor" id="scrollcolor" type="color">
+                    </section>
+                    <button type="submit" id="generate-button" class="pseudo">
+                        Generate Podcast Player
+                    </button>
+                </form> 
+            </main>
+            <aside class="column">
+                <h3>Preview</h3>
+                <div class="preview-window-container pseudo">
+                    <h3 id="preview-window-message">Podcast player will output here once fields are filled and the "generate podcast player" button is clicked.</h3>
+                    <iframe id="preview-window" class="frame-shadow" src="" frameborder="0">Enter</iframe>
+                    <div id="loading-spinner">
+                        <svg version="1.1" id="L7" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+                            <path fill="#fff" d="M31.6,3.5C5.9,13.6-6.6,42.7,3.5,68.4c10.1,25.7,39.2,38.3,64.9,28.1l-3.1-7.9c-21.3,8.4-45.4-2-53.8-23.3
+                            c-8.4-21.3,2-45.4,23.3-53.8L31.6,3.5z">
+                                <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="2s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
+                            </path>
+                            <path fill="#fff" d="M42.3,39.6c5.7-4.3,13.9-3.1,18.1,2.7c4.3,5.7,3.1,13.9-2.7,18.1l4.1,5.5c8.8-6.5,10.6-19,4.1-27.7
+                            c-6.5-8.8-19-10.6-27.7-4.1L42.3,39.6z">
+                                <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="-360 50 50" repeatCount="indefinite"></animateTransform>
+                            </path>
+                            <path fill="#fff" d="M82,35.7C74.1,18,53.4,10.1,35.7,18S10.1,46.6,18,64.3l7.6-3.4c-6-13.5,0-29.3,13.5-35.3s29.3,0,35.3,13.5
+                            L82,35.7z">
+                                <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="2s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+            </aside>
+        </div>
+        <div id="player-codes-container" class="remove">
+            <h3>Podcast Player Codes</h3>
+            <div class="field-sections frame-shadow pseudo output-codes">
+                <section>
+                    <h6>Direct Url</h6>
+                    <a id="direct-url-copy-button" class="pseudo" target="_blank">Open Direct Url</a>
+                </section>
+                <section>
+                    <h6>HTML Embed Code</h6>
+                    <button id="embed-code-copy-button" class="pseudo">Copy Embed Code</button>
+                </section>
+            </div>
+        </div>
+    </div>
+    <script>
+        const fontList = <?php echo json_encode($fonts_list); ?>;
+    </script>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ code-generator/script.js
@@ -0,0 +1,215 @@
+// Url Origin
+
+const podcastPlayerOrigin = 'https://www.partnerwithmagellan.com/podcast-player';
+
+const getRssRoute = 'https://www.partnerwithmagellan.com/wp-json/podcast-player/get-rss-data';
+
+// Data Handling
+
+let rssUrl = null;
+
+let outputUrl = '';
+
+const errHandler = { err: false, msg: null };
+
+let formData = { };
+
+
+// ELEMENT SELECTORS
+
+// Preview Window Selectors
+
+const previewWindow = document.querySelector("#preview-window");
+
+const previewWindowMessage = document.querySelector("#preview-window-message");
+
+// Loading Spinner Selector
+
+const loadingSpinner = document.querySelector("#loading-spinner");
+
+// Monitor Font Field Typing And Render List Selectors
+
+const fontField = document.querySelector("#font");
+
+const fontListOutput = document.querySelector("#font-list-output");
+
+const submitButton = document.querySelector("#generate-button");
+
+// Output Codes Field Selectors
+
+const codesContainer = document.querySelector("#player-codes-container");
+
+const copyUrlButton = document.querySelector("#direct-url-copy-button");
+
+const copyEmbedButton = document.querySelector("#embed-code-copy-button");
+
+
+// EVENT HANDLERS
+
+// Embed Code Button Clipboard Handling
+
+function copyCodeToClipboard() {
+    if (outputUrl) {
+        let outputToClipboard = '';
+        outputToClipboard = `<iframe style="width: 100%; min-height: 37.5rem; max-height: 100%;" src="${outputUrl}"></iframe>`;
+        navigator.clipboard.writeText(outputToClipboard);
+    }
+}
+
+copyEmbedButton.addEventListener("click", copyCodeToClipboard);
+
+// Font Dropdown Clearing
+
+function clearFontListDropdown() {
+    fontListOutput.classList.remove("show");
+    fontListOutput.innerHTML = '';
+    submitButton.classList.remove("hide");
+    fontFieldActive = false;
+}
+
+// Font List Dropdown Handler
+
+let fontFieldActive = false;
+
+fontField.addEventListener("input", () => {
+    const fontOutput = fontList.filter(item => item.toLowerCase().startsWith(fontField.value.toLowerCase()));
+    if (fontOutput.length > 0 && fontField.value !== '') {
+        fontListOutput.classList.add("show");
+        fontFieldActive = true;
+        submitButton.classList.add("hide");
+        fontListOutput.innerHTML = fontOutput.map(font => `<li>${font}</li>`).join("");
+        const fontlistRendered = document.querySelectorAll("#font-list-output li");
+        fontlistRendered.forEach(item => {
+            item.addEventListener("click", () => {
+                fontField.value = item.innerText;
+                clearFontListDropdown();
+            });
+        });
+    } else {
+        clearFontListDropdown()
+    }
+});  
+
+// Render Podcast Player To Preview
+
+async function rssRender(url) {
+    try {
+        codesContainer.classList.add("remove");
+        loadingSpinner.classList.add("show");
+        submitButton.classList.add("hide");
+        previewWindowMessage.innerText = '';
+        const res = await fetch(getRssRoute, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ url })
+        });
+
+        if (res.ok) {
+            errHandler.err = false;
+            rssUrl = await res.json();
+            const fieldsArr = Object.entries(formData).filter(([key, value]) => key !== "url");
+            const urlParams = fieldsArr .map(([key, value], index) => {
+                const outputText = `${key}=${value}`;
+                if (index + 1 === fieldsArr.length) {
+                    return outputText;
+                } else return outputText + "&";
+            }).join("");
+            outputUrl = `${podcastPlayerOrigin}?url=${rssUrl}&${urlParams}`;
+            previewWindow.src = outputUrl;
+            previewWindow.addEventListener("load", () => {
+                loadingSpinner.classList.remove("show");
+                submitButton.classList.remove("hide");
+                copyUrlButton.href= outputUrl;
+                codesContainer.classList.remove("remove");
+            });
+        } else {
+            loadingSpinner.classList.remove("show");
+            codesContainer.classList.add("remove");
+            submitButton.classList.remove("hide");
+            errHandler.err = true;
+            const errMsg = await res.json();
+            errHandler.msg = errMsg.message;
+        }
+    } catch (err) {
+        console.error(err);
+        loadingSpinner.classList.remove("show");
+        codesContainer.classList.add("remove");
+        submitButton.classList.remove("hide");
+        errHandler.err = true;
+        errHandler.msg = 'There was a connection error.  RSS data could not be retrieved.';
+    }
+
+    if (errHandler.err) {
+        loadingSpinner.classList.remove("show");
+        codesContainer.classList.add("remove");
+        submitButton.classList.remove("hide");
+        previewWindow.src = "";
+        previewWindowMessage.innerText = errHandler.msg;
+    }
+}
+
+// Form Submit Handler
+
+function submitHandler() {
+    const formFields = document.querySelectorAll("form input, form select");
+    formFields.forEach(field => {
+        const value = field.value[0] === "#" ? field.value.slice(1) : field.value;
+        formData[field.name] = value;
+    });
+    if (!formData.font) {
+        formData.font = 'Roboto';
+    }
+    const { url } = formData;
+    if (url && url.includes("/podcasts.apple.com/") && url.includes("/id")) {
+        rssRender(url);
+    } else {
+        codesContainer.classList.add("remove");
+        const fieldsErr = "Please enter a valid apple podcast url.";
+        previewWindowMessage.innerText = fieldsErr;
+        previewWindow.src = '';
+    }
+}
+
+// Submit Button Handler
+
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+});
+
+submitButton.addEventListener("click", submitHandler); 
+
+// Key Press Event Handling
+
+window.addEventListener("keyup", e => {
+    if (e.key === "Enter") {
+        submitHandler();
+    }
+    if (e.key === "Tab") {
+        if (!fontList.some(font => font.toLowerCase() === fontField.value.toLowerCase())) {
+            fontField.value = '';
+        }
+        clearFontListDropdown();
+    }
+});
+
+// Clear Url Field Button Click
+
+const rssUrlField = document.querySelector("#url");
+
+document.querySelector("#clear-rss-button").addEventListener("click", () => {
+    rssUrlField.value = '';
+    codesContainer.classList.add("remove");
+});
+
+// Window Click Event Handling
+
+window.addEventListener("click", e => {
+    if (e.target.id !== 'font') {
+        if (!fontList.some(font => font.toLowerCase() === fontField.value.toLowerCase())) {
+            fontField.value = '';
+        }
+        clearFontListDropdown();
+    }
+});
